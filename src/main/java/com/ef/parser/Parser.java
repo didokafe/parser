5de985a1b053b63ff85328f347c7f5352ddc1b73
Daniel Yordanov <didokafe@gmail.com>
2018-03-02 12:24:34 +0200

    Initial commit

---
+++ src/main/java/com/ef/parser/Parser.java
@@ -0,0 +1,14 @@
+package com.ef.parser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@SpringBootApplication
+public class Parser {
+
+	public static void main(String[] args) {
+        System.out.println("$$$$$$$$$$$$$: " + args[0]);
+        System.out.println("$$$$$$$$$$$$$: " + args[1]);
+		//SpringApplication.run(Parser.class, args);
+	}
+}
---
+++ src/test/java/com/ef/parser/ParserTests.java
@@ -0,0 +1,11 @@
+package com.ef.parser;
+
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+public class ParserTests {
+
+	//@Test
+	public void contextLoads() {
+	}
+
+}
