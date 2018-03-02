34e8d7500f5db72cc53fe6c0332fdcd1f1ff1362
Daniel Yordanov <didokafe@gmail.com>
2018-03-02 12:38:22 +0200

    fix

--- src/main/java/com/ef/parser/Parser.java
+++ src/main/java/com/ef/parser/Parser.java
@@ -1,8 +1,5 @@
 package com.ef.parser;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 //@SpringBootApplication
 public class Parser {
 
