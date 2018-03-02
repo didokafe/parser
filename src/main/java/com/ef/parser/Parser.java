b725626079381a066f7954f123a05b2674933ef1
Daniel Yordanov <didokafe@gmail.com>
2018-03-02 17:54:32 +0200

    fix

--- src/main/java/com/ef/parser/Parser.java
+++ src/main/java/com/ef/parser/Parser.java
@@ -1,11 +1,14 @@
 package com.ef.parser;
 
-//@SpringBootApplication
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Parser {
 
 	public static void main(String[] args) {
-        System.out.println("$$$$$$$$$$$$$: " + args[0]);
-        System.out.println("$$$$$$$$$$$$$: " + args[1]);
-		//SpringApplication.run(Parser.class, args);
+        /*System.out.println("$$$$$$$$$$$$$: " + args[0]);
+        System.out.println("$$$$$$$$$$$$$: " + args[1]);*/
+		SpringApplication.run(Parser.class, args);
 	}
 }
