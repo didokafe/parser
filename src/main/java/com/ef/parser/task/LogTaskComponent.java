c4b21379d7c2b1b142a6a1843829af38cff424a9
daniel.yordanov <daniel.yordanov@mentormate.com>
2018-03-04 16:55:12 +0200

    File parsing beginning.

--- src/main/java/com/ef/parser/task/LogTaskComponent.java
+++ src/main/java/com/ef/parser/task/LogTaskComponent.java
@@ -1,12 +1,17 @@
 package com.ef.parser.task;
 
 import com.ef.parser.repository.LogRepository;
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.task.listener.annotation.BeforeTask;
 import org.springframework.cloud.task.repository.TaskExecution;
 import org.springframework.stereotype.Component;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
 
 @Component
 public class LogTaskComponent {
@@ -16,10 +21,38 @@ public class LogTaskComponent {
     @Autowired
     private LogRepository logRepository;
 
+    private String filePath;
+
 
     @BeforeTask
     public void findLogs(TaskExecution taskExecution) {
-        //logger.info("task running..." + taskExecution.getArguments().get(0));
+        resolveArgs(taskExecution.getArguments());
+        File file = new File(filePath);
+        try {
+            List<String> lines = FileUtils.readLines(file, "UTF-8");
+            lines.forEach(line -> {
+                logger.info("task running..." + line);
+
+            });
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         logRepository.findAll();
+    }
+
+    //switch to resolve arguments
+    private void resolveArgs(List<String> args) {
+        args.forEach(arg -> {
+            String[] argPart = arg.split("=");
+            switch(argPart[0]) {
+                case("--accesslog"):
+                    filePath = argPart[1];
+                    break;
+                default:
+                    break;
+            }
+        });
     }
 }
--- tools/environment.sh
+++ tools/environment.sh
@@ -1,2 +1,8 @@
-mysql -u root -proot;
-create database parser;
+#!/bin/bash
+
+#mysql -u root -proot;
+#create database parser;
+
+
+mvn clean package
+java -jar target/parser.jar --accesslog=/home/dido/Dropbox/Java_MySQL_Test/Java_MySQL_Test_Instructions.txt
